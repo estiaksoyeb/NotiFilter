2a8791bf0456c77e29d325cd62b11110ac8adc6c
estiaksoyeb <estiakahamed122@gmail.com>
2026-02-18 14:17:56 +0600

    style: Use system default fonts instead of Fira Mono

--- app/src/main/java/co/adityarajput/notifilter/views/Theme.kt
+++ app/src/main/java/co/adityarajput/notifilter/views/Theme.kt
@@ -5,10 +5,6 @@ import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import co.adityarajput.notifilter.R
 
 private val ColorScheme = darkColorScheme(
     primary = Color(0xFF7C70FF), // Purple
@@ -18,32 +14,6 @@ private val ColorScheme = darkColorScheme(
 
 val Orange = Color(0xFFffad7d)
 
-private val Typography = Typography().run {
-    val firaMono = FontFamily(
-        Font(R.font.firamono_regular, FontWeight.Normal),
-        Font(R.font.firamono_medium, FontWeight.Medium),
-        Font(R.font.firamono_bold, FontWeight.Bold),
-    )
-
-    Typography(
-        displayLarge.copy(fontFamily = firaMono),
-        displayMedium.copy(fontFamily = firaMono),
-        displaySmall.copy(fontFamily = firaMono),
-        headlineLarge.copy(fontFamily = firaMono),
-        headlineMedium.copy(fontFamily = firaMono),
-        headlineSmall.copy(fontFamily = firaMono),
-        titleLarge.copy(fontFamily = firaMono),
-        titleMedium.copy(fontFamily = firaMono),
-        titleSmall.copy(fontFamily = firaMono),
-        bodyLarge.copy(fontFamily = firaMono),
-        bodyMedium.copy(fontFamily = firaMono),
-        bodySmall.copy(fontFamily = firaMono),
-        labelLarge.copy(fontFamily = firaMono),
-        labelMedium.copy(fontFamily = firaMono),
-        labelSmall.copy(fontFamily = firaMono),
-    )
-}
-
 @Composable
 fun Theme(content: @Composable () -> Unit) =
-    MaterialTheme(ColorScheme, MaterialTheme.shapes, Typography, content)
+    MaterialTheme(ColorScheme, MaterialTheme.shapes, Typography(), content)
